c73a04b668cdb474452a0ae8e0f553d8d8157bde
eduamdev <35645733+eduamdev@users.noreply.github.com>
2024-03-09 14:54:34 -0600

    chores: add inter variable font from fontsource

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ function App() {
     <>
       <div>
         <h1>Hello world</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
+          provident cupiditate quidem at illum quis eligendi aut ullam
+          dignissimos recusandae!
+        </p>
       </div>
     </>
   );
