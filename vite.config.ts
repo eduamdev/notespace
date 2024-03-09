f02e62e492ffb9acf539bdcf8721f47e67aa85a6
eduamdev <35645733+eduamdev@users.noreply.github.com>
2024-03-08 22:17:55 -0600

    chore: add dev-dist folder to gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
       manifest: {
         name: "NoteGuard",
         short_name: "NoteGuard",
