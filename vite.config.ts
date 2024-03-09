5a1f4dcb6b6df6d2b9ec1f66977c4f262050dd85
eduamdev <35645733+eduamdev@users.noreply.github.com>
2024-03-08 21:05:39 -0600

    feat: add PWA support using vite-plugin-pwa and add new favicons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "NoteGuard",
+        short_name: "NoteGuard",
+        description:
+          "NoteGuard is your go-to digital notebook for effortless note-taking, organizing thoughts, and staying productive on-the-go. Enjoy secure, encrypted notes, offline access, and seamless synchronization across all your devices",
+        icons: [
+          { src: "/icon-192.png", type: "image/png", sizes: "192x192" },
+          { src: "/icon-512.png", type: "image/png", sizes: "512x512" },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#FFFFFF",
+        theme_color: "#FFFFFF",
+      },
+    }),
+  ],
+});
