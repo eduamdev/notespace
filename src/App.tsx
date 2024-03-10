457909125599e3363496daf042463041a2698083
eduamdev <35645733+eduamdev@users.noreply.github.com>
2024-03-10 16:28:27 -0600

    chores: initial UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <div>
-        <h1>Hello world</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
-          provident cupiditate quidem at illum quis eligendi aut ullam
-          dignissimos recusandae!
-        </p>
+    <div className="flex flex-col items-center">
+      <div className="w-full max-w-none px-5 sm:max-w-[90%] sm:px-0">
+        <h1 className="pt-6 text-2xl font-semibold">NoteGuard</h1>
+        {/* actions */}
+        <div className="flex flex-row gap-6 pt-12">
+          <button>Add note</button>
+          <button>Add folder</button>
+        </div>
+        <div className="pt-6">
+          <div className="flex items-center justify-between">
+            <span>1. Folder 1</span>
+            {/* actions */}
+            <div className="flex flex-row items-center gap-x-5">
+              <button>edit</button>
+              <button>delete</button>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <span>2. Folder 2</span>
+            {/* actions */}
+            <div className="flex flex-row items-center gap-x-5">
+              <button>edit</button>
+              <button>delete</button>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <span>3. Folder 3</span>
+            {/* actions */}
+            <div className="flex flex-row items-center gap-x-5">
+              <button>edit</button>
+              <button>delete</button>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <span>4. Folder 4</span>
+            {/* actions */}
+            <div className="flex flex-row items-center gap-x-5">
+              <button>edit</button>
+              <button>delete</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
