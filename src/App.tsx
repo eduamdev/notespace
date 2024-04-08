4842249165e2e2db3ba31f33bd52f8623ca71aa2
eduamdev <35645733+eduamdev@users.noreply.github.com>
2024-04-07 19:23:56 -0600

    feat: add note detail component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Login from "@/components/auth/login";
 import Register from "@/components/auth/register";
 import NoteList from "@/components/note/note-list";
 import NoteEditor from "@/components/note/note-editor";
-import CreateNoteForm from "@/components/note/create-note-form";
+import NoteDetail from "@/components/note/note-detail";
 
 function App() {
   return (
@@ -24,8 +24,8 @@ function App() {
 
         {/* Note Routes */}
         <Route path="/notes/all" component={NoteList} />
-        <Route path="/notes/new" component={CreateNoteForm} />
-        <Route path="/notes/:id" component={NoteEditor} />
+        <Route path="/notes/new" component={NoteEditor} />
+        <Route path="/notes/:id" component={NoteDetail} />
 
         {/* Default Route (404) */}
         <Route>
--- src/components/note/note-editor.tsx
+++ src/components/note/note-editor.tsx
@@ -1,8 +1,57 @@
+import React, { useState } from "react";
+import { createNote } from "@/services/note-service";
+
 function NoteEditor() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await createNote(title, content);
+      // Reset form fields
+      setTitle("");
+      setContent("");
+
+      alert("success!");
+    } catch (error) {
+      console.error("Error creating note:", error);
+    }
+  };
+
   return (
-    <>
-      <h1 className="py-3 text-xl font-semibold">Note Editor</h1>
-    </>
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    <form onSubmit={handleSubmit}>
+      <div className="flex flex-col gap-2">
+        <h1 className="py-3 text-xl font-semibold">New note</h1>
+        <input
+          type="text"
+          value={title}
+          className="border p-2"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          placeholder="Title"
+        />
+        <textarea
+          className="border p-2"
+          rows={10}
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+          placeholder="Content"
+        ></textarea>
+        <div className="mt-4">
+          <button
+            type="submit"
+            className="inline-flex bg-black p-3 font-medium text-white"
+          >
+            Create Note
+          </button>
+        </div>
+      </div>
+    </form>
   );
 }
 
