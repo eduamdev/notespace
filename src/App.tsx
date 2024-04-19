8294f18b3462ba81b15e564c0375705e6bece9c2
eduamdev <35645733+eduamdev@users.noreply.github.com>
2024-04-19 04:57:05 -0600

    add private routes and check for internet connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Switch } from "wouter";
+import { useEffect, useState } from "react";
+import { Redirect, Route, Switch } from "wouter";
 import Layout from "@/components/layout/layout";
 
 import Login from "@/components/auth/login";
@@ -8,24 +9,54 @@ import NoteEditor from "@/components/note/note-editor";
 import NoteDetail from "@/components/note/note-detail";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    // TODO: implement the token logic
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token); // Set isLoggedIn based on token existence
+
+    const handleOnlineStatus = () => setIsOnline(navigator.onLine);
+
+    window.addEventListener("online", handleOnlineStatus);
+    window.addEventListener("offline", handleOnlineStatus);
+
+    return () => {
+      window.removeEventListener("online", handleOnlineStatus);
+      window.removeEventListener("offline", handleOnlineStatus);
+    };
+  }, []);
+
+  // Function to render private routes
+  const PrivateRoute = ({
+    component: Component,
+    ...rest
+  }: {
+    component: React.ComponentType<any>;
+    [x: string]: any;
+  }) => (
+    <Route {...rest}>
+      {isLoggedIn ? <Component /> : <Redirect to="/login" />}
+    </Route>
+  );
+
   return (
     <Layout>
+      {isOnline ? null : (
+        <div className="text-center bg-yellow-300">No internet connection</div>
+      )}
       <Switch>
-        <Route path="/">
-          {/* Home Page */}
-          <div>
-            <h1 className="py-3 text-xl font-semibold">Home</h1>
-          </div>
-        </Route>
+        <Route path="/" component={() => <Redirect to="/notes/all" />} />
 
         {/* Auth Routes */}
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
 
-        {/* Note Routes */}
-        <Route path="/notes/all" component={NoteList} />
-        <Route path="/notes/new" component={NoteEditor} />
-        <Route path="/notes/:id" component={NoteDetail} />
+        {/* Private Routes */}
+        <PrivateRoute path="/notes/all" component={NoteList} />
+        <PrivateRoute path="/notes/new" component={NoteEditor} />
+        <PrivateRoute path="/notes/:id" component={NoteDetail} />
 
         {/* Default Route (404) */}
         <Route>
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,12 +3,13 @@ import { Link } from "wouter";
 function Header() {
   return (
     <>
-      <div className="pt-6 text-2xl font-semibold">NoteGuard</div>
+      <div className="pt-6 text-2xl font-semibold">
+        <Link href="/">NoteGuard</Link>
+      </div>
+
       <nav className="flex flex-row gap-x-3 py-4">
-        <Link href="/">Home</Link>
         <Link href="/login">Login</Link>
         <Link href="/register">Register</Link>
-        <Link href="/notes/all">All notes</Link>
         <Link href="/notes/new">New note</Link>
       </nav>
     </>
