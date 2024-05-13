661feeedd697434ef35d1828a6f881f29522a9fd
eduamdev <35645733+eduamdev@users.noreply.github.com>
2024-05-12 21:42:51 -0600

    ui improvements

--- src/components/note/note-list.tsx
+++ src/components/note/note-list.tsx
@@ -57,26 +57,29 @@ function NoteList() {
       <div className="flex items-center justify-between px-6 py-4">
         <h1 className="text-lg font-semibold text-black">Notes</h1>
         <div className="flex items-center justify-center gap-4">
-          <button>
-            <SearchIcon className="size-5" />
-          </button>
           <Link
             href="/notes/new"
-            className="flex h-[34px] items-center rounded-lg bg-cyan-600 pl-1.5 pr-2.5 text-cyan-50"
+            className="flex h-[34px] items-center justify-center rounded-lg bg-cyan-600 pl-1.5 pr-2.5 text-cyan-50"
           >
             <PlusIcon className="inline-block size-4" />
             <span className="pl-1.5 text-sm font-medium">Note</span>
           </Link>
         </div>
       </div>
-      <ul className="space-y-4 px-6 py-4">
+      <div className="px-6 py-2">
+        <button className="h-10 w-full rounded-md border border-black/[0.12] px-3 shadow-sm shadow-black/[0.08]">
+          <SearchIcon className="size-[18px]" />
+        </button>
+      </div>
+      <ul className="divide-y py-4">
         {notes.map((note) => (
-          <li key={note.id}>
-            <Link to={`/notes/${note.id}`} className="block w-full">
+          <li key={note.id} className="py-1">
+            <Link to={`/notes/${note.id}`} className="block w-full px-6">
               <p className="truncate text-[15px] font-medium text-black">
                 {note.title}
               </p>
               <p className="line-clamp-2 text-[15px]">{note.content}</p>
+              <p className="text-[13px] text-neutral-500">Now</p>
             </Link>
           </li>
         ))}
