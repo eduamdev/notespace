38ee1ea68d8e307e698af8a460e5224388255660
eduamdev <35645733+eduamdev@users.noreply.github.com>
2024-05-27 23:33:49 -0600

    fix eslint error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,4 +33,9 @@ module.exports = {
     project: ["./tsconfig.json", "./tsconfig.node.json"],
     tsconfigRootDir: __dirname,
   },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 };
