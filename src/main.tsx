41acba9e5737da9c1345e4e43f8d7df38fe71c0f
eduamdev <35645733+eduamdev@users.noreply.github.com>
2024-06-11 22:27:25 -0600

    install react-query-devtools and update pnpm packages

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "@fontsource-variable/public-sans/wght-italic.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { EncryptionProvider } from "@/contexts/encryption-context";
 import App from "@/App";
 import "@/styles/globals.css";
@@ -26,6 +27,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <EncryptionProvider>
         <App />
+        <ReactQueryDevtools />
       </EncryptionProvider>
     </QueryClientProvider>
   </React.StrictMode>
